4e40f3adc2034d0083dfba771ccbb32b65ffe535
WassimSss <tojiikan@gmail.com>
2024-06-11 11:38:43 +0200

    fix get workouts day

--- components/ui/SelectWithLabel.tsx
+++ components/ui/SelectWithLabel.tsx
@@ -35,7 +35,7 @@ export default function SelectWithLabel({ label, name, required, className, opti
   return (<div>
     <label htmlFor={name} className={`block mb-2 text-sm font-medium text-gray-900 dark:text-white ${className}`}>{label}</label>
     <select id={name} required={required} disabled={!active} onChange={(e: React.ChangeEvent<HTMLSelectElement>) => (onChange(e.target.value))} className={`${active ? "cursor-pointer bg-gray-50 border-gray-300 text-gray-900" : "bg-neutral-600 text-neutral-400 border-neutral-700"}   border    text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}>
-      {active ? optionJSX : <option value="" disabled selected>Non disponible</option>}
+      {active ? optionJSX : <option value="" disabled defaultValue={'null'}>Non disponible</option>}
     </select>
   </div >);
 }

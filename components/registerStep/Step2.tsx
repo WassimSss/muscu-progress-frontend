408abb13342a535e3c79a992712d79b068dc535b
WassimSss <tojiikan@gmail.com>
2024-07-09 14:20:52 +0200

    step 2

--- components/registerStep/Step2.tsx
+++ components/registerStep/Step2.tsx
@@ -51,26 +51,32 @@ export default function Step1({ handleStep }: Step1Props) {
 
   const allPrograms = programs.map((program, i) => {
     const currentProgram = selectedProgram === program.value;
+
     return (
-      <CarouselItem key={i}>
-        <div className={`border-2 ${currentProgram ? "border-primary" : "border-gray-600"}  p-6 text-white bg-neutral-800 flex flex-col items-center justify-between`}>
-          <MinusCircle size={32} />
-          <p className="text-2xl">{program.title}</p>
-          <p className="text-base text-gray-400 text-center">
-            {program.description}
-          </p>
-
-          {/* <Button className={`${currentProgram && "cursor-default bg-neutral-600 text-neutral-300"}`} onClick={() => handleChoseProgram(program.value)}>
-            Choisir ce programme
-          </Button> */}
-
-
-        </div>
-      </CarouselItem >
+      <div key={i} className="size-24 bg-neutral-700">
+        <h1 className="text-2xl">{program.title}</h1>
+        <p className="text-base text-gray-400 text-center">
+          {program.description}
+        </p>
+      </div>
     );
   });
 
+  // <CarouselItem key={i}>
+  //   <div className={`border-2 ${currentProgram ? "border-primary" : "border-gray-600"}  p-6 text-white bg-neutral-800 flex flex-col items-center justify-between`}>
+  //     <MinusCircle size={32} />
+  //     <p className="text-2xl">{program.title}</p>
+  //     <p className="text-base text-gray-400 text-center">
+  //       {program.description}
+  //     </p>
 
+  //     {/* <Button className={`${currentProgram && "cursor-default bg-neutral-600 text-neutral-300"}`} onClick={() => handleChoseProgram(program.value)}>
+  //       Choisir ce programme
+  //     </Button> */}
+
+
+  //   </div>
+  // </CarouselItem >
 
   const handleSubmit = async () => {
     if (!email || !password || !confirmPassword) {
@@ -123,14 +129,10 @@ export default function Step1({ handleStep }: Step1Props) {
           <p className="text-sm text-gray-500 dark:text-gray-400">Choisissez le programme qui vous convient le mieux</p>
           <div className="space-y-4 md:space-y-6">
             {/* <form onClick={(e) => handleSubmit(e)}> */}
+            <div className="flex flex-wrap justify-around">
+              {allPrograms}
+            </div>
 
-            <Carousel className="w-full">
-              <CarouselContent>
-                {allPrograms}
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
 
             <button onClick={() => handleSubmit()} className="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
             {/* </form> */}
